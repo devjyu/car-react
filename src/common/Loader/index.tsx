43017ad002b7b619986499cf01b20aca03d49143
devjyu <devjyu@gmail.com>
2024-09-11 16:35:11 +0900

    first commit

---
+++ src/common/Loader/index.tsx
@@ -0,0 +1,17 @@
+import { ScaleLoader } from "react-spinners";
+
+const Loader = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-white">
+      {/* <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div> */}
+      <ScaleLoader
+        color="#2241F2"
+        height={25}
+        speedMultiplier={1}
+        width={13}
+      />
+    </div>
+  );
+};
+
+export default Loader;
